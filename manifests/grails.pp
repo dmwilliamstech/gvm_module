724fcf739f0ded283ebd3f67461075ce708bb510
dmwilliams.tech@gmail.com <dmwilliams.tech@gmail.com>
2013-11-15 10:33:50 -0500

    Added grails and groovy

--- manifests/grails.pp
+++ manifests/grails.pp
@@ -1,12 +1,10 @@
 class gvm::grails($version = '1.3.7'){
 
     exec{'install-grails':
-        command=>'source /home/vagrant/.gvm/bin/gvm-init.sh && gvm install grails -v $version',
+        command=>'gvm install grails -v $version',
         cwd =>"/home/vagrant",
         logoutput=>true,
-        path=> ["/usr/bin", "/usr/sbin", "/bin", "/home/vagrant/.gvm/bin"],
-	require=> Class['gvm']
+        path=> ["/usr/bin", "/usr/sbin", "/bin", "/.gvm/bin"],
+        require=> Class['gvm']
 }
  }
-
-
--- manifests/groovy.pp
+++ manifests/groovy.pp
@@ -1,9 +1,9 @@
-class gvm::groovy($version = '1.8.8'){
+lass gvm::groovy($version = '1.8.8'){
     exec{'install-groovy':
-        command=>'source /home/vagrant/.gvm/bin/gvm-init.sh && gvm install groovy -v $version',
+        command=>'gvm install groovy -v $version',
         cwd=> "/home/vagrant",
         logoutput=>true,
-        path=> ["/usr/bin", "/usr/sbin", "/bin", "/home/vagrant/.gvm/bin"],
-	require=> Class['gvm']
+        path=> ["/usr/bin", "/usr/sbin", "/bin", "/.gvm/bin"],
+        require=> Class['gvm']
 }
 }
