c57ada8a7b985472de0ef7edb24b94523b1a7854
dmwilliams.tech@gmail.com <dmwilliams.tech@gmail.com>
2013-11-15 13:09:29 -0500

    file reference

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,28 +5,26 @@ class gvm{
 
 exec {'set_java_home': 
 	require=>Package['java-1.6.0-openjdk'], 
-	command=>"/bin/echo 'export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0.x86_64/jre' >> /home/vagrant/.bashrc"
+	command=>"/bin/echo 'export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0.x86_64/jre' >> /home/vagrant/.bashrc",
+}->
+package{'curl':
+        ensure => installed,
+        provider=>'yum'
 }->
 
-if ! defined(Package['curl']) 
-
-{package 
-	{'curl': 
-	ensure => installed, 
-	provider=>'yum'} 
-}
-
-if ! defined(Package['unzip']) 
-{package 
-	{'unzip': 
-	ensure => installed, 
-	provider=>'yum'}}
-
-exec {'install_curl': cwd=>"/home/vagrant/", path=>['/bin/sh', '/bin/bash', '/usr/bin', '/bin', '/usr/sbin'], command=>"cat .bashrc", onlyif=>"bash -c '/usr/bin/curl -s get.gvmtool.net | bash'"}->
+package{'unzip':
+	ensure => installed,
+        provider=>'yum'
+}->
+exec {'install_curl': 
+	cwd=>"/home/vagrant/", 
+	path=>['/bin/sh', '/bin/bash', '/usr/bin', '/bin', '/usr/sbin'], 
+	command=>"cat .bashrc", onlyif=>"bash -c '/usr/bin/curl -s get.gvmtool.net | bash'"
+}->
 file{'/tmp/gvm_source.sh':
 	ensure=>present,
 	mode=>'0777',
-	source=>'puppet:///modules/gvm_module/files/gvm_source.sh'
+	source=>'puppet:///modules/gvm/files/gvm_source.sh'
 }->
 exec {'/tmp/gvm_source.sh': 
 	path=>['/bin/sh', '/bin/bash', '/usr/bin', '/bin', '/usr/sbin'],
