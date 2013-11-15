56112ef637abf9f4d593a116efe59e4405418e73
dmwilliams.tech@gmail.com <dmwilliams.tech@gmail.com>
2013-11-15 10:21:35 -0500

    Restructured gvm module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,13 @@
-class gvm($install_gvm=true) {
-	stage{'gvm-install':}
-  if $install_gvm {
-    class {
-      'gvm::dependencies': stage => 'gvm-install'
-    }
-}
-}
+package {'java-1.6.0-openjdk': ensure => installed }->
+
+exec {'set_java_home': require=>Package['java-1.6.0-openjdk'], command=>"/bin/echo 'export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0.x86_64/jre' >> /home/vagrant/.bashrc"}->
+
+package{'unzip': ensure=>'present', provider=>'yum'}->
+
+package {'curl': ensure=>'present', provider=>'yum'}->
+
+exec {'install_curl': cwd=>"/home/vagrant/", path=>['/bin/sh', '/bin/bash', '/usr/bin', '/bin', '/usr/sbin'], command=>"cat .bashrc", onlyif=>"bash -c '/usr/bin/curl -s get.gvmtool.net | bash'"}->
+
+exec {'source_gvm_init': path=>['/bin/sh', '/bin/bash', '/usr/bin', '/bin', '/usr/sbin'], command=>"sudo /tmp/vagrant-puppet/manifests/gvm_source.sh"}->
+
+exec {'source_bashrc': path=>['/bin/sh', '/bin/bash', '/usr/bin', '/bin', '/usr/sbin'], command=>'sudo bash -c /home/vagrant/.bashrc'}
