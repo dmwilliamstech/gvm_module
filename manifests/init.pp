a66a3f612c5c4fb744659bc764ec1f3a22426cce
dmwilliamstech <dmwilliams.tech@gmail.com>
2013-11-15 10:30:22 -0500

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,4 @@
+class gvm{
 package {'java-1.6.0-openjdk': ensure => installed }->
 
 exec {'set_java_home': require=>Package['java-1.6.0-openjdk'], command=>"/bin/echo 'export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0.x86_64/jre' >> /home/vagrant/.bashrc"}->
@@ -11,3 +12,4 @@ exec {'install_curl': cwd=>"/home/vagrant/", path=>['/bin/sh', '/bin/bash', '/us
 exec {'source_gvm_init': path=>['/bin/sh', '/bin/bash', '/usr/bin', '/bin', '/usr/sbin'], command=>"sudo /tmp/vagrant-puppet/manifests/gvm_source.sh"}->
 
 exec {'source_bashrc': path=>['/bin/sh', '/bin/bash', '/usr/bin', '/bin', '/usr/sbin'], command=>'sudo bash -c /home/vagrant/.bashrc'}
+}
